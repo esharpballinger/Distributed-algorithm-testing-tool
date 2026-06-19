df9b73e45ae4d4f23228991e8f353e88abaf0c86
esharpballinger <esharpballinger06@gmail.com>
2026-06-19 16:38:21 -0400

    Made edits to functions in node.py, added function for work to be done when receiving a message, added method in node to change the data

--- src/node.py
+++ src/node.py
@@ -1,18 +1,21 @@
 class Node:
 
     self_work: function = None
+    work_after_receiving: function = None
     data = None
     id: int = None
 
-    def __init__(self, self_work, data):
+    def __init__(self, self_work, data, receiving_func) -> None:
         """
         Initialize self work function and the data
         :param self_work: A function pointer to the work that the node can do between rounds for free
         :param data: The data that the node has access to
         """
-        pass
+        self.self_work = self_work
+        self.data = data
+        self.work_after_receiving = receiving_func
     
-    def send_message(self, message, recipient: Node):
+    def send_message(self, message, recipient: Node) -> None:
         """
         Sends a message to a recipient node
         :param message: the message to send
@@ -20,19 +23,21 @@ class Node:
         """
         recipient.receive_message(message, self)
 
-    def receive_message(self, message, sender):
+    def receive_message(self, message, sender) -> None:
         """
-        (Work in progress, there is likely a better way to do this)
         Recieve message from another node
         :param message: The recieved message
         :param sender: the node that sent the message
         """
-        # Do some work, send a response, etc
+        self.work_after_receiving(self, message)
         pass
 
-    def do_work(self):
+    def do_work(self) -> None:
         """
         Does work for "free" using the function pointer contained in the node
         """
-        # Will use the function pointer in some way
-        pass
+        self.self_work(self.data)
+
+    def change_data(self, new_data):
+        self.data = new_data
+        return self.data

f38aed5141cde50823ae6fbb12be2ce357a20601
esharpballinger <esharpballinger06@gmail.com>
2026-07-26 03:17:10 -0400

    Added testing file to git

---
+++ tests/test_ccliquesim.py
@@ -0,0 +1,6 @@
+from src.ccliquesim import *
+import maximal_independent_set as mis
+
+if __name__ == "__main__":
+    sim = CCSim(mis.GreedyMISInit, 10, "src/goober.txt", "output/goober.csv")
+    sim.run_sims
